636a3adebcf41a9e631880a8b8565777b90ede5b
spark-o-petit <petitspark247@gmail.com>
2024-12-30 00:38:47 +0900

    Initial Commit

---
+++ MySwiftCalculator/MySwiftCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftCalculatorApp.swift
+//  MySwiftCalculator
+//
+//  Created by alex on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySwiftCalculatorTests/MySwiftCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftCalculatorTests.swift
+//  MySwiftCalculatorTests
+//
+//  Created by alex on 12/30/24.
+//
+
+import Testing
+@testable import MySwiftCalculator
+
+struct MySwiftCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
