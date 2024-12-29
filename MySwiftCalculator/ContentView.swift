a8eacbf8445725b86b8e17055f267e91299faa6c
spark-o-petit <petitspark247@gmail.com>
2024-12-30 01:47:11 +0900

    Refactor calculator UI: clean up repetaed code of button structure

--- MySwiftCalculator/ContentView.swift
+++ MySwiftCalculator/ContentView.swift
@@ -7,16 +7,60 @@
 
 import SwiftUI
 
+enum ButtonType:String {
+  case first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, zero
+  case dot, equal, plus, minus, multiply, divide
+  case percent, opposite, clear
+  
+  var buttonDisplayName: String {
+    switch self {
+    case .first: return "1"
+    case .second: return "2"
+    case .third: return "3"
+    case .fourth: return "4"
+    case .fifth: return "5"
+    case .sixth: return "6"
+    case .seventh: return "7"
+    case .eighth: return "8"
+    case .ninth: return "9"
+    case .zero: return "0"
+    case .clear: return "C"
+    case .dot: return "."
+    case .equal: return "="
+    case .plus: return "+"
+    case .minus: return "-"
+    case .multiply: return "*"
+    case .divide: return "/"
+    case .percent: return "%"
+    case .opposite: return "+/-"
+    
+    }
+  }
+  var backgroundColor: Color {
+    switch self {
+    case .first, .second, .third, .fourth, .fifth, .sixth, .seventh, .eighth, .ninth, .zero, .dot : return Color("NumberButton")
+    case .equal,.plus, .minus, .multiply, .divide : return Color.orange
+    case .percent, .opposite, .clear: return Color.gray
+    }
+  }
+  var foregroundColor: Color {
+    switch self {
+    case .percent, .opposite, .clear: return Color.black
+      default : return Color.white
+    }
+  }
+}
+
 struct ContentView: View {
   
   @State private var totalNumber: String = "0"
   
-  private let buttonData: [[String]] = [
-    ["C", "/", "%", "$"],
-    ["7", "8", "9", "X"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", "0", ".", "="]
+  private let buttonData: [[ButtonType]] = [
+    [.clear, .opposite, .percent, .divide],
+    [.seventh, .eighth, .ninth, .multiply],
+    [.fourth, .fifth, .sixth, .minus],
+    [.first, .second, .third, .plus],
+    [.zero, .dot, .equal]
   ]
   
   var body: some View {
@@ -34,7 +78,6 @@ struct ContentView: View {
         
         ForEach(buttonData, id: \.self) {row in
           HStack {
-            
             ForEach(row, id: \.self) {button in
               Button {
                 if totalNumber == "0" {
@@ -43,21 +86,16 @@ struct ContentView: View {
                   totalNumber += "7"
                 }
               } label: {
-                Text("7")
+                Text(button.buttonDisplayName)
                   .frame(width: 80, height: 80)
-                  .background(Color.numberButton)
+                  .background(button.backgroundColor)
                   .cornerRadius(40)
-                  .foregroundColor(Color.white)
+                  .foregroundColor(button.foregroundColor)
                   .font(Font.system(size: 40))
               }
             }
-            
           }
-          
-          
-          
         }
-        .padding()
       }
     }
   }
