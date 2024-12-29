c4b5c396dd68128f90577872876b25ee89db98f9
spark-o-petit <petitspark247@gmail.com>
2024-12-30 01:18:48 +0900

    Refactor calculator UI: clean up button structure and prepare for functional logic

--- MySwiftCalculator/ContentView.swift
+++ MySwiftCalculator/ContentView.swift
@@ -11,163 +11,56 @@ struct ContentView: View {
   
   @State private var totalNumber: String = "0"
   
-    var body: some View {
-      ZStack {
-        Color.black.edgesIgnoringSafeArea(.all)
-        VStack {
+  private let buttonData: [[String]] = [
+    ["C", "/", "%", "$"],
+    ["7", "8", "9", "X"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", "0", ".", "="]
+  ]
+  
+  var body: some View {
+    ZStack {
+      Color.black.edgesIgnoringSafeArea(.all)
+      VStack {
+        Spacer()
+        HStack {
           Spacer()
-          HStack {
-            Spacer()
-            Text(totalNumber)
-              .padding()
-              .font(Font.system(size: 73))
-              .foregroundColor(Color.white)
-          }
-          HStack {
-            
-            Button {
-              totalNumber = "0"
-            } label: {
-              Text("C")
-                .frame(width: 80, height: 80)
-                .background(Color.gray)
-                .cornerRadius(40)
-                .foregroundColor(Color.black)
-                .font(Font.system(size: 40))
-            }
-
-            Text("/")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.black)
-              .font(Font.system(size: 40))
-            Text("%")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.black)
-              .font(Font.system(size: 40))
-            Text("%")
-              .frame(width: 80, height: 80)
-              .background(Color.orange)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-          }
+          Text(totalNumber)
+            .padding()
+            .font(Font.system(size: 73))
+            .foregroundColor(Color.white)
+        }
+        
+        ForEach(buttonData, id: \.self) {row in
           HStack {
             
-            Button {
-              if totalNumber == "0" {
-                totalNumber = "7"
-              } else {
-                totalNumber += "7"
+            ForEach(row, id: \.self) {button in
+              Button {
+                if totalNumber == "0" {
+                  totalNumber = "7"
+                } else {
+                  totalNumber += "7"
+                }
+              } label: {
+                Text("7")
+                  .frame(width: 80, height: 80)
+                  .background(Color.numberButton)
+                  .cornerRadius(40)
+                  .foregroundColor(Color.white)
+                  .font(Font.system(size: 40))
               }
-            } label: {
-              Text("7")
-                .frame(width: 80, height: 80)
-                .background(Color.numberButton)
-                .cornerRadius(40)
-                .foregroundColor(Color.white)
-                .font(Font.system(size: 40))
             }
-
-            Text("8")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("9")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("X")
-              .frame(width: 80, height: 80)
-              .background(Color.orange)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
+            
           }
-          HStack {
-            Text("4")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("5")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("6")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("-")
-              .frame(width: 80, height: 80)
-              .background(Color.orange)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-          }
-          HStack {
-            Text("1")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("2")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("3")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("+")
-              .frame(width: 80, height: 80)
-              .background(Color.orange)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-          }
-          HStack {
-            Text("0")
-              .frame(width: 170, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text(".")
-              .frame(width: 80, height: 80)
-              .background(Color.gray)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-            Text("=")
-              .frame(width: 80, height: 80)
-              .background(Color.orange)
-              .cornerRadius(40)
-              .foregroundColor(Color.white)
-              .font(Font.system(size: 40))
-          }
-
+          
+          
+          
         }
         .padding()
       }
     }
+  }
 }
 
 #Preview {
